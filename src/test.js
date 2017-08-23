64c2991c5495ac8c151c1f7bcc41f5a3f0b85655
iuvo <alpha@iuvo.ai>
2017-08-23 18:21:58 +0000

    replace with better commit message

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 var marmota = new Marmota();
 { manca(); var iarba_verde = 10; }
-var ciocolate = getCiocolate();
+let ciocolate = getCiocolate();
 function traba() {
   for (var ciocolata in ciocolate) {
     marmota.inveleste(ciocolata, "staniol")
