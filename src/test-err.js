7d8ccb1fa4712d1b9d018a0404670289cc58f663
Cosmin Radoi <cosmin.radoi@gmail.com>
2017-10-14 07:50:59 -0700

    Create test-err.js

---
+++ src/test-err.js
@@ -0,0 +1,9 @@
+var marmota = new Marmota();
+{ manca(); let iarba_verde = 10
+let ciocolate = getCiocolate();
+function traba() {
+  for (var ciocolata in ciocolate) {
+    marmota.inveleste(ciocolata, "staniol")
+  }
+  fericita(marmota);
+}
