607642b9c90725c40c94e815221c399ffe9fc472
Cosmin Radoi <cosmin.radoi@gmail.com>
2019-05-04 16:55:49 -0700

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,6 +1,3 @@
-var marmota = new Marmota();
-{ manca(); var iarba_verde = 10; }
-var ciocolate = getCiocolate();
 function traba() {
   for (var ciocolata in ciocolate) {
     marmota.inveleste(ciocolata, "staniol")
