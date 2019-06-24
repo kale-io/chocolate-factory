cfd87c2be4926b73aa97c2505af1da3f8e9a036f
cos <cosmin.radoi@gmail.com>
2019-06-24 14:59:03 -0700

    more files

---
+++ src/test.6.js
@@ -0,0 +1,11 @@
+var marmota = new Marmota(6);
+{ manca(); var iarba_verde = 10; }
+var ciocolate = getCiocolate();
+function traba() {
+  for (var ciocolata in ciocolate) {
+    if(foo == bar)
+      marmota.inveleste(ciocolata, "staniol")
+  }
+  fericita(marmota);
+}
+var f = function(buz) { return buz; }
